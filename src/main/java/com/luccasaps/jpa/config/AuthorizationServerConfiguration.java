f4b9c6488fec38d39abb8a851ac9d90f9ccb154b
Luccas Asaphe <167210535+LuccasAps@users.noreply.github.com>
2025-04-29 12:48:51 -0300

    Obtendo token JWT e voltando versão do spring boot de 3.4.4. para 3.3.4 para funcionar o uthServerSecurityFilterChain da classe AuthorizationServerConfiguration

--- src/main/java/com/luccasaps/jpa/config/AuthorizationServerConfiguration.java
+++ src/main/java/com/luccasaps/jpa/config/AuthorizationServerConfiguration.java
@@ -1,38 +1,50 @@
 package com.luccasaps.jpa.config;
 
+import com.nimbusds.jose.jwk.JWKSet;
+import com.nimbusds.jose.jwk.RSAKey;
+import com.nimbusds.jose.jwk.source.ImmutableJWKSet;
+import com.nimbusds.jose.jwk.source.JWKSource;
+import com.nimbusds.jose.proc.SecurityContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configurers.AuthorizeHttpRequestsConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.server.authorization.config.annotation.web.configuration.OAuth2AuthorizationServerConfiguration;
 import org.springframework.security.oauth2.server.authorization.config.annotation.web.configurers.OAuth2AuthorizationServerConfigurer;
 import org.springframework.security.oauth2.server.authorization.settings.ClientSettings;
 import org.springframework.security.oauth2.server.authorization.settings.OAuth2TokenFormat;
 import org.springframework.security.oauth2.server.authorization.settings.TokenSettings;
 import org.springframework.security.web.SecurityFilterChain;
 
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+import java.security.interfaces.RSAPrivateCrtKey;
+import java.security.interfaces.RSAPublicKey;
 import java.time.Duration;
+import java.util.UUID;
 
 @Configuration
 @EnableWebSecurity
 public class AuthorizationServerConfiguration {
 
-    @Bean
     @Order(1)
+    @Bean
     public SecurityFilterChain authServerSecurityFilterChain(HttpSecurity http) throws Exception {
 
-        //OAuth2AuthorizationServerConfiguration.applyDefaultSecurity(); ⇐ method "applyDefaultSecurity()" is deprecated
+        OAuth2AuthorizationServerConfiguration.applyDefaultSecurity(http); //⇐ method "applyDefaultSecurity()" is deprecated
 
-        http.with(OAuth2AuthorizationServerConfigurer.authorizationServer(), Customizer.withDefaults()); // ⇐ solução do metodo deprecated
+        //http.with(OAuth2AuthorizationServerConfigurer.authorizationServer(), Customizer.withDefaults()); // ⇐ solução do metodo deprecated
 
         http.getConfigurer(OAuth2AuthorizationServerConfigurer.class)
                 .oidc(Customizer.withDefaults());
 
-        http.oauth2ResourceServer(oauth2Rs -> oauth2Rs.jwt(Customizer.withDefaults()));
+        http.oauth2ResourceServer(
+                oauth2Rs -> oauth2Rs.jwt(Customizer.withDefaults()));
 
         http.formLogin(configurer -> configurer.loginPage("/login"));
 
@@ -57,5 +69,33 @@ public class AuthorizationServerConfiguration {
         return ClientSettings.builder()
                 .requireAuthorizationConsent(false)
                 .build();
+    }
+
+    //JWK - JSON Web Key
+    @Bean
+    public JWKSource<SecurityContext> jwkSource() throws Exception {
+        RSAKey key = gerarChaveRSA();
+        JWKSet jwkSet = new JWKSet(key);
+        return new ImmutableJWKSet<>(jwkSet);
+    }
+
+    // Gerar par de chaves RSA
+    private RSAKey gerarChaveRSA() throws Exception {
+        KeyPairGenerator keyPairGenerator = KeyPairGenerator.getInstance("RSA");
+        keyPairGenerator.initialize(2048);
+        KeyPair keyPair = keyPairGenerator.generateKeyPair();
+
+        RSAPublicKey chavePublica = (RSAPublicKey) keyPair.getPublic();
+        RSAPrivateCrtKey chavePrivada = (RSAPrivateCrtKey) keyPair.getPrivate();
+
+        return new RSAKey.Builder(chavePublica)
+                .privateKey(chavePrivada)
+                .keyID(UUID.randomUUID().toString())
+                .build();
+    }
+
+    @Bean
+    public JwtDecoder jwtDecoder(JWKSource<SecurityContext> jwkSource) {
+        return OAuth2AuthorizationServerConfiguration.jwtDecoder(jwkSource);
     }
 }
