36f75f4ac10e7d66f011c2b3777fc85325b4e331
Luccas Asaphe <167210535+LuccasAps@users.noreply.github.com>
2025-06-02 12:47:33 -0300

    Criando ambiente de produção e parametrizando variareis de ambiente

--- src/main/java/com/luccasaps/jpa/config/DatabaseConfiguration.java
+++ src/main/java/com/luccasaps/jpa/config/DatabaseConfiguration.java
@@ -1,9 +1,17 @@
 package com.luccasaps.jpa.config;
 
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+import javax.sql.DataSource;
 
 @Configuration
+@Slf4j
 public class DatabaseConfiguration {
 
 
@@ -21,33 +29,36 @@ public class DatabaseConfiguration {
 
 
     //@Bean
-//    public DataSource dataSource(){
-//        DriverManagerDataSource ds = new DriverManagerDataSource();
-//
-//        ds.setUrl(url);
-//        ds.setUsername(username);
-//        ds.setPassword(password);
-//        ds.setDriverClassName(driver);
-//
-//        return ds;
-//    }
+    public DataSource dataSource(){
+        DriverManagerDataSource ds = new DriverManagerDataSource();
 
-    //@Bean
-//    public DataSource hikariDataSource(){
-//        HikariConfig config = new HikariConfig();
-//        config.setJdbcUrl(url);
-//        config.setUsername(username);
-//        config.setPassword(password);
-//        config.setDriverClassName(driver);
-//
-//        config.setMaximumPoolSize(10); // maximo de conexões
-//        config.setMinimumIdle(1); // tamaho inicial do pool
-//        config.setPoolName("library-db-pool");
-//        config.setMaxLifetime(600000);//10 minutos (600 mil ms)
-//        config.setConnectionTimeout(60000);//1 minuto (60 mil ms)
-//        config.setConnectionTestQuery("SELECT 1");//query teste
-//
-//        return new HikariDataSource(config);
-//    }
+        ds.setUrl(url);
+        ds.setUsername(username);
+        ds.setPassword(password);
+        ds.setDriverClassName(driver);
+
+        return ds;
+    }
+
+    @Bean
+    public DataSource hikariDataSource(){
+
+        log.info("Iniciando conexão com o banco na URL: {}", url);
+
+        HikariConfig config = new HikariConfig();
+        config.setUsername(username);
+        config.setPassword(password);
+        config.setDriverClassName(driver);
+        config.setJdbcUrl(url);
+
+        config.setMaximumPoolSize(10); // maximo de conexões
+        config.setMinimumIdle(1); // tamaho inicial do pool
+        config.setPoolName("library-db-pool");
+        config.setMaxLifetime(600000);//10 minutos (600 mil ms)
+        config.setConnectionTimeout(60000);//1 minuto (60 mil ms)
+        config.setConnectionTestQuery("SELECT 1");//query teste
+
+        return new HikariDataSource(config);
+    }
 
 }
